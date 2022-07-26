965867ccab77fecf33a00b463eb2fe19696c719c
jhahn678 <jhahn678@yahoo.com>
2022-07-26 14:21:31 -0400

    Added reference to parent waterbody on geometry model.

--- models/geometry.js
+++ models/geometry.js
@@ -5,6 +5,10 @@ const geometrySchema = new mongoose.Schema({
     name: String,
     classification: String,
     states: [String],
+    parent_waterbody: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Waterbody'
+    },
     geometry: {
         type: {
             type: String,
