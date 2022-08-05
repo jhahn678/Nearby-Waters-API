9f746e29e5dcd961677eeb3154f34590dde77680
jhahn678 <jhahn678@yahoo.com>
2022-08-04 23:07:40 -0400

    Cleaned up the aggregation pipelines

--- src/routes/autocomplete.ts
+++ src/routes/autocomplete.ts
@@ -2,10 +2,9 @@ import { Router } from 'express'
 import controllers from '../controllers'
 const router = Router()
 
-router.get('/autocomplete', controllers.autocompleteAll)
-router.get('/autocomplete/geoplaces', controllers.autocompletePlaces)
-router.get('/autocomplete/waterbodies', controllers.autocompleteWaterbodies)
-router.get('/state', controllers.getStateByCoords)
+router.get('/', controllers.autocompleteAll)
+router.get('/geoplaces', controllers.autocompletePlaces)
+router.get('/waterbodies', controllers.autocompleteWaterbodies)
 
 
 export default router;
