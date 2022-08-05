136222eca52cd938a22ff5e8e797ef6b6d46ea1d
jhahn678 <jhahn678@yahoo.com>
2022-08-04 23:08:39 -0400

    Added weighted aggregation pipeline for waterbodies && added pagination

--- src/controllers/waterbody.ts
+++ src/controllers/waterbody.ts
@@ -3,7 +3,9 @@ import catchAsync from "../utils/catchAsync";
 import { validateCoords } from '../utils/coordValidation'
 import { milesToMeters } from "../utils/conversions";
 import { Request } from "express";
-import { FilterQuery } from 'mongoose'
+import { FilterQuery, PipelineStage } from 'mongoose'
+import { distanceWeightFunction } from "../utils/searchWeights";
+import { CoordinateError } from "../utils/errors/CoordinateError";
 
 
 interface WaterbodyQuery {
@@ -53,18 +55,23 @@ interface WaterbodiesQuery {
      * Number of miles
      */
     within?: string | number
+    page: string
+    limit: string
 }
 
-export const getWaterbodies = catchAsync(async (req: Request<{},{},{},WaterbodiesQuery>, res, next) => {
 
+export const getWaterbodies = catchAsync(async(req: Request<{},{},{},WaterbodiesQuery>, res, next) => {
     const { 
         value, classifications, 
         states, ccode, subregion, 
         minWeight, maxWeight,
-        geometries, lnglat, within 
+        geometries, lnglat, within,
+        page, limit 
     } = req.query;
 
-    const filters: FilterQuery<IWaterbody>[] = []
+    const filters:  FilterQuery<IWaterbody>[] = []
+
+    const pipeline: PipelineStage[] = []
 
     if(value){
         filters.push({ name: { $regex: `^${value}`, options: 'i' }})
@@ -77,11 +84,16 @@ export const getWaterbodies = catchAsync(async (req: Request<{},{},{},Waterbodie
         const split = classifications.split(',').map(x => x.trim())
         filters.push({ classification: { $in: split }})
     }
-    if(minWeight){
-        filters.push({ weight: { $gte: parseFloat(minWeight) }})
+    if(minWeight && maxWeight){
+        filters.push({ weight: { 
+            $and: [{ $gte: parseFloat(minWeight) }, { $lte: parseFloat(maxWeight) }] 
+        }})
     }
-    if(maxWeight){
+    else if(maxWeight){
         filters.push({ weight: { $lte: parseFloat(maxWeight) }})
+    }
+    else if(minWeight){
+        filters.push({ weight: { $gte: parseFloat(minWeight) }})
     }
     if(ccode){
         filters.push({ ccode: ccode })
@@ -89,36 +101,76 @@ export const getWaterbodies = catchAsync(async (req: Request<{},{},{},Waterbodie
     if(subregion){
         filters.push({ subregion: subregion })
     }
+
     if(lnglat){
         const coords = lnglat.split(',').map(x => parseFloat(x))
-        if(validateCoords(coords)){
-            const geometry = { type: 'Point', coordinates: coords }
-                filters.push({ 
-                    simplified_geometries: { 
-                        $near: { 
-                            $geometry: geometry, 
-                            $maxDistance: within ? milesToMeters(within) : milesToMeters(50)
-                        }
-                    }
-                })
+        if(!validateCoords(coords)){
+            throw new CoordinateError(400, 'Provided coordinates are not valid')
         }
+        pipeline.push(
+            { $geoNear: { 
+                near: { type: "Point", coordinates: [coords[0], coords[1]] },
+                distanceField: 'distanceFrom',
+                query: { $and: filters.length > 0 ? filters : [{}] },
+                maxDistance: within ? milesToMeters(within) : 80000,
+                spherical: false
+            }},
+            { $addFields: { 
+                rank: { $function: {
+                    body: within ? 
+                        distanceWeightFunction(milesToMeters(within)) : 
+                        distanceWeightFunction(80000),
+                    args: [ '$distanceFrom', '$weight'],
+                    lang: 'js'
+                }}
+            }}
+        )
+    }else{
+        pipeline.push( 
+            { $match: { $and: filters.length > 0 ? filters : [{}] } },
+            { $addFields: { rank: '$weight', distanceFrom: 0 } },
+        )
     }
+
+    const projection: PipelineStage.FacetPipelineStage[] = []
+    
 
     if(geometries){
-        const results = await Waterbody
-            .find({ $and: filters })
-            .populate('geometries')
-            .lean()
-            .sort({ weight: -1 })
-
-        res.status(200).json(results)
-    }else{
-        const results = await Waterbody
-            .find({ $and: filters })
-            .lean()
-            .sort({ weight: -1 })
-
-        res.status(200).json(results)
+        projection.push({
+            $lookup: {
+                from: 'geometries',
+                localField: 'geometries',
+                foreignField: '_id',
+                as: 'geometries'
+            }
+        })
     }
 
+    projection.push({ $project: { simplified_geometries: 0 } })
+           
+
+    pipeline.push({ 
+        $facet: {
+            metadata: [ 
+                { $count: "total" }, 
+                { $addFields: { page: parseInt(page) || 1 } } 
+            ],
+            data: [ 
+                { $sort: { rank: -1 } },
+                { $skip: (parseInt(limit) * (parseInt(page) - 1)) || 0 }, 
+                { $limit: parseInt(limit) || 50 },
+                ...projection
+            ]
+        }
+    })
+    
+
+    const result = await Waterbody.aggregate(pipeline)
+
+
+    res.status(200).json({
+        metadata: result[0].metadata[0],
+        data: result[0].data
+    })
 })
+
