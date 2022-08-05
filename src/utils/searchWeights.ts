9857380fa3463208d2b8c60815b85536d0251df3
jhahn678 <jhahn678@yahoo.com>
2022-08-04 23:06:55 -0400

    Added some ts comments and included additional distance params

--- src/utils/searchWeights.ts
+++ src/utils/searchWeights.ts
@@ -1,19 +1,34 @@
-export const distanceWeight = ( distance: number, maxDistance=500000, maxWeight=2, minWeight=1.3 ): number => {
+export const distanceWeight = ( distance: number, maxDistance=300000, maxWeight=2, minWeight=1.3 ): number => {
 // weight = maxweight - ( ( distance / maxDistance )^2 * ( maxweight - minweight ) )
     return (
         maxWeight - ( Math.pow((distance/maxDistance), 2) * ( maxWeight - minWeight ) )   
     )
 }
 
-type DistanceWeightFunction = (maxDistance?: number, maxWeight?: number, minWeight?: number) => (
-    distance: number, weight: number, maxDistance?: number, maxWeight?: number, minWeight?: number
+type DistanceWeightFunction = (
+    /** Maximum distance in meters for weight to be applied to
+     *  -- should line up with query distance 
+     * @default 300000 */
+    maxDistance?: number, 
+    /** Minimum distance in meters for weight to be applied to  
+     * @default 15000 */
+    minDistance?: number,
+    /** Maximum weight for a waterbody by distance
+    * @default 2 */
+    maxWeight?: number, 
+    /** Minimum weight for a waterbody by distance 
+    *  -- lines up with maxDistance
+    * @default 1.3 */
+    minWeight?: number
+    ) => (
+    distance: number, weight: number, maxDistance?: number, minDistance?: number, maxWeight?: number, minWeight?: number
 ) => number
 
 export const distanceWeightFunction: DistanceWeightFunction = () => (
-    distance, weight, maxDistance=500000, maxWeight=2, minWeight=1.3
+    distance, weight, maxDistance=300000, minDistance=15000, maxWeight=2, minWeight=1.3
 ) => {
     return (
-        (maxWeight - ( Math.pow(((distance < 15000 ? 15000 : distance)/maxDistance), 2) * ( maxWeight - minWeight ) )) * weight
+        (maxWeight - ( Math.pow(((distance < minDistance ? minDistance : distance)/maxDistance), 2) * ( maxWeight - minWeight ) )) * weight
     )
 }
 
