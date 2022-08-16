de188c5ea06b842258f83ed90f6f18c959c60a09
jhahn678 <jhahn678@yahoo.com>
2022-08-16 10:25:35 -0400

    Updated error to include reference to model

--- src/utils/errors/UnknownReferenceError.ts
+++ src/utils/errors/UnknownReferenceError.ts
@@ -1,14 +1,21 @@
+type ReferenceType = 
+| 'WATERBODY' | 'GEOMETRY' | "GEOPLACE"
+
 export class UnknownReferenceError extends Error {
     status: number = 400
     message: string
+    referenceType: ReferenceType | undefined
     unknownReferences: string[]
 
-    constructor(references: string[] = []){
+    constructor(referenceType?: ReferenceType, references: string[] = []){
         super()
+        if(referenceType){
+            this.referenceType = referenceType
+        }
         this.unknownReferences = references
         if(references.length > 0){
             this.message = `
-                Could not find the following references:
+                Could not find the following ${referenceType} references:
                 ${references.join(', ')}
             `
         }else{
