d25343f8568773dedde301b0b09e52e3547d8149
jhahn678 <jhahn678@yahoo.com>
2022-08-27 23:43:55 -0400

    Better type guarding

--- src/utils/conversions.ts
+++ src/utils/conversions.ts
@@ -1,7 +1,9 @@
 export const milesToMeters = (miles: number | string): number => {
     if(typeof miles === 'string'){
         return Math.floor(parseInt(miles) * 1609.33999997549)
-    }else{
+    }else if(typeof miles === 'number'){
         return Math.floor(miles * 1609.33999997549)
+    }else{
+        return Math.floor(50 * 1609.33999997549)
     }
 }
