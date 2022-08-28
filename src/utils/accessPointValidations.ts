5e0b29fa120d00794dc8f1d140a6788abfc68d95
jhahn678 <jhahn678@yahoo.com>
2022-08-27 23:46:06 -0400

    refactoring

--- src/utils/accessPointValidations.ts
+++
@@ -1,7 +0,0 @@
-export const validateAccessPointType = (type: string): boolean => {
-    if(type === 'PARKING_LOT' || type === 'PULLOFF' || type === 'WALK_IN'){
-        return true;
-    }else{
-        return false;
-    }
-}
--- src/utils/adminOneValidation.ts
+++
@@ -1,8 +0,0 @@
-import { AdminOneName, ADMIN_ONE_MAP } from "../types/enums/adminOne"
-
-
-export const validateAdminOne = (value: string): AdminOneName | null  => {
-    const name = ADMIN_ONE_MAP[value.toLowerCase()];
-    if(name) return name;
-    return null;
-}
